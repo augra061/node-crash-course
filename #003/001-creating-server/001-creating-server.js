02f33c5cb5079ec1aa9b16500b63977b05e542b4
unknown <augra061@gmail.com>
2020-07-06 23:42:01 -0300

    Files from lesson 03 removed

--- #003/001-creating-server/001-creating-server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    return console.log('request made');
-});
-
-server.listen(3000, 'localhost', () => {
-    return console.log('listening for requests on port 3000');
-});
